b96daf00dfdac26e9b9850e31e66052ad748edcf
Vishnu Gopal <vishnu@mobme.in>
2009-06-24 21:58:31 +0530

    A working inspector HUD style.

--- protoinspector.js
+++ protoinspector.js
@@ -6,6 +6,20 @@ var ProtoInspector = Class.create({
 	class_name: "protoinspector-default",
 	
 	initialize: function(options) {
-		
+		this.options = options;
+		this.initializeInspector();
+	},
+	
+	initializeInspector: function() {
+		this.inspector = $(this.options.id);
+		this.inspector.addClassName(this.class_name);
+		new Draggable(this.inspector);
+		this.inspector.hide();
+	},
+	
+	show: function(options) {
+		this.inspector.style.left = options.left + "px";
+		this.inspector.style.top = options.top + "px";
+		this.inspector.show();
 	}
-};
+});
