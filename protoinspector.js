776fd60e949c1e72ef12d0102f5ac6599f5dc0a2
Vishnu Gopal <vishnu@mobme.in>
2009-06-24 20:47:08 +0530

    Initial commit of protoinspector.

---
+++ protoinspector.js
@@ -0,0 +1,11 @@
+
+/* Creates a HUD style inspector element that is great for making
+	 OSX-like inspector windows. */
+
+var ProtoInspector = Class.create({
+	class_name: "protoinspector-default",
+	
+	initialize: function(options) {
+		
+	}
+};
